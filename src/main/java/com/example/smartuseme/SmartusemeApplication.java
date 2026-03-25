0d28bad66a48895a274699487d0b9a114365d7b4
C BHANU PRAKASH <bhanu18042005@gmail.com>
2026-03-26 04:52:45 +0530

    intialized springboot with maven

---
+++ src/main/java/com/example/smartuseme/SmartusemeApplication.java
@@ -0,0 +1,13 @@
+package com.example.smartuseme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartusemeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartusemeApplication.class, args);
+	}
+
+}
